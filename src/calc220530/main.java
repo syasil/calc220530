f0e7cabf22225fda5303e3aff54de9fcf6a99737
syasil <syasil2402@gmail.com>
2022-05-31 16:59:52 +0900

    create main pannel

--- src/calc220530/main.java
+++ src/calc220530/main.java
@@ -1,8 +1,16 @@
 package calc220530;
 
+import java.awt.Frame;
+
+
 public class main {
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		Frame mainFrame = new Frame();
+		
+		mainFrame.setTitle("메인 프레임입니다.");//창 제목을 설정
+		mainFrame.setBounds(100,100,300,300); //창 크기를 설정 x,y,width,height
+		mainFrame.setVisible(true);
+		
 	}
 
 }
